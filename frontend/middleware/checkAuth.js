5a879ed730970ca4f270128c7b175f9202fb4420
TywinEvergreen <holydiver.gs@gmail.com>
2021-01-29 14:10:38 +0300

    обновлен requrements.txt

--- frontend/middleware/checkAuth.js
+++ frontend/middleware/checkAuth.js
@@ -1,5 +1,7 @@
 export default function ({ store, route, app }) {
     const auth = store.state.user.isAuthenticated;
+    console.log(auth);
+    console.log(route);
     if (!auth && route.name != 'user-register') {
         app.router.push('user/login')
     }
