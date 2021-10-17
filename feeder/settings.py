7f3a02d06ac6fb30f6465bf0cdc13b7ea96cf8ae
gs <holydiver.gs@gmail.com>
2021-10-17 20:28:40 +0300

    Minor change

--- feeder/settings.py
+++ feeder/settings.py
@@ -92,12 +92,7 @@ WSGI_APPLICATION = 'feeder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        os.getenv('DATABASE_URL')
     }
 }
 
