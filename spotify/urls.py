d27fe3985d7a8a6274f68a7f43b0cd287bedbb1b
gs <holydiver.gs@gmail.com>
2022-03-10 22:14:14 +0300

    Tiny bit of refactoring

--- spotify/urls.py
+++ spotify/urls.py
@@ -1,5 +1,3 @@
-from django.conf.urls import url
-from django.urls import include
 from rest_framework import routers
 
 from spotify.views import ArtistViewSet, NewAlbumsViewSet
@@ -12,6 +10,4 @@ router = routers.DefaultRouter()
 router.register('artist', ArtistViewSet, basename='artist')
 router.register('album-notifications', NewAlbumsViewSet, basename='album-notifications')
 
-urlpatterns = [
-    url('', include(router.urls))
-]
+urlpatterns = router.urls
