9a9ba37f3f5fe8bc5c47961d0a583755a45d540b
gs <holydiver.gs@gmail.com>
2022-03-12 22:00:26 +0300

    Add blank connect function

---
+++ spotify/views/connect.py
@@ -0,0 +1,11 @@
+from rest_framework import mixins
+from rest_framework.views import APIView
+
+
+class ConnectSpotifyAccountApiView(APIView):
+    """
+    Connect user's spotify account to the service
+    """
+
+    def post(self) -> None:
+        pass
